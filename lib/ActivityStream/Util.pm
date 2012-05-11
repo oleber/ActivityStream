1f4e4e02851d7618ba2f509d0d26acba6c8b9653
Oleber <oleber@gmail.com>
2012-05-11 22:38:18 +0200

    Better ActivityStream::Util::generate_id

--- lib/ActivityStream/Util.pm
+++ lib/ActivityStream/Util.pm
@@ -5,6 +5,7 @@ use warnings;
 
 use Digest::MD5 qw(md5_base64);
 use Data::UUID;
+use MIME::Base64;
 use Readonly;
 
 use ActivityStream::Constants;
@@ -12,9 +13,10 @@ use ActivityStream::Constants;
 {
     my $root = join( ',', Data::UUID->new->create_str(), $$, time, $ActivityStream::Constants::GENERATE_ID_SECRET );
     sub generate_id {
-        $root = time . md5_base64( $root . $$ . time . $ActivityStream::Constants::GENERATE_ID_SECRET );
+        $root = encode_base64( reverse(pack('I',time)), '' ) . md5_base64( $root . $$ . time . $ActivityStream::Constants::GENERATE_ID_SECRET );
+        $root =~ s/=//g;
         $root =~ tr/0123456789\/\+/abcdefghijkl/;
-        return $root;
+        return substr($root, 0, 20);
     }
 }
 
